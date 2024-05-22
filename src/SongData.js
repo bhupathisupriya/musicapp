3c195ee537da62285ac21a6c836b02b0c7c57ff4
supriya <bhupathisupriya28@gmail.com>
2024-05-22 08:05:35 +0530

    message

--- src/SongData.js
+++ src/SongData.js
@@ -6,7 +6,7 @@ function data() {
       audio:
         "https://mp3teluguwap.net/mp3/2024/Devara/All%20Hail%20The%20Tiger.mp3",
       name: "All Hail The Tiger",
-      artist:"Anirudh Ravichander, Natalie Di Luccio",
+      artist: "Anirudh Ravichander, Natalie Di Luccio",
       id: uuidv4(),
       color: ["#EF8EA9", "#ab417f"],
       active: true,
@@ -18,23 +18,100 @@ function data() {
         "https://mp3teluguwap.net/mp3/2024/Pushpa2/Pushpa2/Pushpa%20Pushpa.mp3",
       name: "Pushpa Pushpa Song (Full) ",
       id: uuidv4(),
-      artist:"Nakash Aziz",
+      artist: "Nakash Aziz",
       color: ["#EF8EA9", "#ab417f"],
       active: false,
       cover:
         "https://naasongs.com.co/wp-content/uploads/2023/04/Pushpa-2-2023-Songs.jpg",
-    },,
+    },
     {
       audio:
         "https://mp3teluguwap.net/mp3/2024/Family%20Star/Family%20Star/Kalyani%20Vaccha%20Vacchaa.mp3",
       name: "Kalyani Vaccha Vacchaa Song",
       id: uuidv4(),
       active: false,
-      artist:"Karthik, Mangli",
+      artist: "Karthik, Mangli",
       color: ["#EF8EA9", "#ab417f"],
       cover:
         "https://naasongs.com.co/wp-content/uploads/2024/03/The-Family-Star-2024.jpg",
     },
+    {
+      audio:
+        "https://sencloud.online/mp3/Telugu%20Mp3/atoz/2000to2009/O/Oy%21%20%282009%29/03%20-%20Waiting%20For%20You%20-%20SenSongsMp3.co.mp3",
+      name: "anukoledhanadu Song",
+      id: uuidv4(),
+      active: false,
+      artist: "Shreya Ghoshal",
+      color: ["#EF8EA9", "#ab417f"],
+      cover:
+        "https://naasongs.com.co/wp-content/uploads/2018/11/Oy-2009jpeg-300x300.jpg",
+    },
+    {
+      audio:
+        "https://mp3teluguwap.net/mp3/2023/Kushi%20(2023)/Kushi%20(2023)%20-%20HQ/Na%20Roja%20Nuvve.mp3",
+      name: "narojanuvve Song",
+      id: uuidv4(),
+      active: false,
+      artist: "Hesham Abdul Wahab, Manju Sri",
+      color: ["#EF8EA9", "#ab417f"],
+      cover:
+        "https://naasongs.com.co/wp-content/uploads/2023/07/Kushi-2023.jpg",
+    },
+    {
+      audio:
+        "https://sencloud.online/mp3/Telugu%20Mp3/All/Parugu(2008)/Nammavemo-SenSongsMp3.Com.mp3",
+      name: "nammavemo Song",
+      id: uuidv4(),
+      active: false,
+      artist: "Saketh",
+      color: ["#EF8EA9", "#ab417f"],
+      cover:
+        "https://naasongs.com.co/wp-content/uploads/2018/06/Parugu-2008jpeg-300x300.jpg",
+    },
+    {
+      audio:
+        "https://mp3teluguwap.net/mp3/2017/Mahanubavudu%20-%20(2017)/Mahanubavudu%20(2017)%20-%20HQ/Rendu%20Kallu%20-%20SenSongsMp3.Co.mp3",
+      name: "rendukallu Song",
+      id: uuidv4(),
+      active: false,
+      artist: "Armaan Malik",
+      color: ["#EF8EA9", "#ab417f"],
+      cover:
+        "https://naasongs.com.co/wp-content/uploads/2018/05/Mahanubavudu-2017jpeg-300x300.jpg",
+    },
+    {
+      audio:
+        "https://sencloud.online/mp3/Telugu%20Mp3/2016/Hyper%20(2016)/3-Ompula%20Dhaniya-SenSongsMp3.Co.mp3",
+      name: "ompulahaniya Song",
+      id: uuidv4(),
+      active: false,
+      artist: "Gibran",
+      color: ["#EF8EA9", "#ab417f"],
+      cover:
+        "https://naasongs.com.co/wp-content/uploads/2018/05/Hyper-2016jpeg-300x300.jpg",
+    },
+    {
+      audio:
+        "https://mp3teluguwap.net/mp3/2023/Skanda/Skanda%20-%20HQ/Dummare%20Dumma.mp3",
+      name: "dummare dumma Song",
+      id: uuidv4(),
+      active: false,
+      artist: "Armaan Malik, Ayyan Pranathi",
+      color: ["#EF8EA9", "#ab417f"],
+      cover:
+        "https://naasongs.com.co/wp-content/uploads/2023/08/Skanda-2023.jpg",  
+    },
+    {
+      audio:
+        "https://sencloud.online/mp3/Telugu%20Mp3/old/Nagarjuna/Sri%20Ramadasu%20%282006%29/Thandrimata-SenSongsMp3.Co.mp3",
+      name: "thandrimata Song",
+      id: uuidv4(),
+      active: false,
+      artist: "Sunitha",
+      color: ["#EF8EA9", "#ab417f"],
+      cover:
+        "https://naasongs.com.co/wp-content/uploads/2018/10/Sri-Ramadasu-2006jpeg-300x300.jpg",  
+    }
   ];
 }
 
